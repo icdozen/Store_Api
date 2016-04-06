815a59bb31a53e93adc6ac0e9e4c464790820490
icdozen <icdozen@gmail.com>
2016-04-06 12:36:48 -0400

    Adds shoulda matchers for spec refactors

---
+++ lib/spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
+  it { should validate_presence_of(:email) }
+it { should validate_uniqueness_of(:email) }
+it { should validate_confirmation_of(:password) }
+it { should allow_value('example@domain.com').for(:email) }
+end
