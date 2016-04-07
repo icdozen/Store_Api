a9955116dcb76e14144004501c22643540381037
icdozen <icdozen@gmail.com>
2016-04-07 10:11:54 -0400

    lib user fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
-
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+end
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
 end
